4b7639bc8291686fe31164855c82d92b2fc99408
Harry Ng <harryworld@gmail.com>
2014-07-18 06:46:45 +0800

    Add new files

---
+++ AudioDemo/controllers/DEMORootViewController.h
@@ -0,0 +1,13 @@
+//
+//  DEMORootViewController.h
+//  AudioDemo
+//
+//  Created by Harry Ng on 18/7/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import "REFrostedContainerViewController.h"
+
+@interface DEMORootViewController : REFrostedContainerViewController
+
+@end
