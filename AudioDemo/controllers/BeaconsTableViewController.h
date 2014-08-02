d485e8b417e8d76b39dfbfa5140ee44d76579056
Harry Ng <harryworld@gmail.com>
2014-08-02 23:49:36 +0800

    Add BeaconsTableViewController

---
+++ AudioDemo/controllers/BeaconsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BeaconsTableViewController.h
+//  AudioDemo
+//
+//  Created by Harry Ng on 2/8/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BeaconsTableViewController : UITableViewController
+
+@end
